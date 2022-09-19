7a201d1fcfb6d3371ccaab0526adf0ebc82c4344
r_boo <r_boo@192.168.1.65>
2022-09-18 17:56:54 -0700

    Finish Exceptions Demo

---
+++ src/_00_Intro_to_Exceptions/NegativeNumberException.java
@@ -0,0 +1,9 @@
+package _00_Intro_to_Exceptions;
+
+import javax.swing.JOptionPane;
+
+public class NegativeNumberException extends Exception {
+ void scaryPopup() {
+	 JOptionPane.showMessageDialog(null, " Warning Following Critical Error Has Been Found \njavax.internal.critical.error.corruption.crash.i14");
+ }
+}
